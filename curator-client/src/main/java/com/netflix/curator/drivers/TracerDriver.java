140068af232c80e750fd861665bf22b4bcc6bcee
cesar.alba <cesar.alba@blue-tc.com>
2012-11-20 12:01:08 +0100

    Format

--- curator-client/src/main/java/com/netflix/curator/drivers/TracerDriver.java
+++ curator-client/src/main/java/com/netflix/curator/drivers/TracerDriver.java
@@ -22,22 +22,26 @@ import java.util.concurrent.TimeUnit;
 /**
  * Mechanism for timing methods and recording counters
  */
-public interface TracerDriver
-{
-    /**
-     * Record the given trace event
-     *
-     * @param name of the event
-     * @param time time event took
-     * @param unit time unit
-     */
-    public void     addTrace(String name, long time, TimeUnit unit);
+public interface TracerDriver {
+	/**
+	 * Record the given trace event
+	 *
+	 * @param name
+	 *            of the event
+	 * @param time
+	 *            time event took
+	 * @param unit
+	 *            time unit
+	 */
+	public void addTrace(String name, long time, TimeUnit unit);
 
-    /**
-     * Add to a named counter
-     *
-     * @param name name of the counter
-     * @param increment amount to increment
-     */
-    public void     addCount(String name, int increment);
+	/**
+	 * Add to a named counter
+	 *
+	 * @param name
+	 *            name of the counter
+	 * @param increment
+	 *            amount to increment
+	 */
+	public void addCount(String name, int increment);
 }
